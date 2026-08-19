01616b43e7d1d394daf7b9dac910ba380bbdf87d
Malik Smith <malikux@proton.me>
2026-08-19 12:23:41 +0000

    Changed-DemographicCard

--- src/components/ecommerce/DemographicCard.tsx
+++ src/components/ecommerce/DemographicCard.tsx
@@ -19,7 +19,7 @@ export default function DemographicCard() {
       <div className="flex justify-between">
         <div>
           <h3 className="text-lg font-semibold text-gray-800 dark:text-white/90">
-            Customers Demographic
+            Employees
           </h3>
           <p className="mt-1 text-gray-500 text-theme-sm dark:text-gray-400">
             Number of customer based on country
