4fc466d6877e026404f157eeee9e1a03fd018024
Malik Smith <malikux@proton.me>
2026-08-21 10:33:47 +0000

    Changed profile details  # Please enter the commit message for your changes. Lines starting

--- src/components/UserProfile/UserInfoCard.tsx
+++ src/components/UserProfile/UserInfoCard.tsx
@@ -25,7 +25,7 @@ export default function UserInfoCard() {
                 First Name
               </p>
               <p className="text-sm font-medium text-gray-800 dark:text-white/90">
-                Musharof
+                Alex
               </p>
             </div>
 
@@ -34,7 +34,7 @@ export default function UserInfoCard() {
                 Last Name
               </p>
               <p className="text-sm font-medium text-gray-800 dark:text-white/90">
-                Chowdhury
+                Thomas
               </p>
             </div>
 
@@ -43,7 +43,7 @@ export default function UserInfoCard() {
                 Email address
               </p>
               <p className="text-sm font-medium text-gray-800 dark:text-white/90">
-                randomuser@pimjo.com
+                 alex.thomas@apporg.com
               </p>
             </div>
 
