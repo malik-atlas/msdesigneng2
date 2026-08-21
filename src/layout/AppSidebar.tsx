7c1cc7d75dd6c5a73a181e2c36fe578e8cb8954e
Malik Smith <malikux@proton.me>
2026-08-21 09:50:13 +0000

    Fixed-sidebar-width

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -288,7 +288,7 @@ const AppSidebar: React.FC = () => {
       className={`fixed mt-16 flex flex-col lg:mt-0 top-0 px-5 left-0 bg-white dark:bg-gray-900 dark:border-gray-800 text-gray-900 h-screen transition-all duration-300 ease-in-out z-50 border-r border-gray-200 
         ${
           isExpanded || isMobileOpen
-            ? "w-[210px]"
+            ? "w-[290px]"
             : isHovered
             ? "w-[290px]"
             : "w-[90px]"
